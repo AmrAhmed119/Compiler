a3b1ba36fc23ac1b7cb052c67ac695b7f02d32bd
Hussein Mohamed <hussain.mansour2019@gmail.com>
2024-12-23 00:11:59 +0200

    finish parser table

--- ParserGenerator/ParserTable/ParserTable.cpp
+++ ParserGenerator/ParserTable/ParserTable.cpp
@@ -1,1 +1,34 @@
+#include <stdexcept>
 #include "ParserTable.h"
+
+
+ParserTable::ParserTable(std::map<std::string, NonTerminal> &nonTerminals) : nonTerminals(nonTerminals) {}
+
+void ParserTable::parserTableCreator() {
+    for (auto &nonTerminal : nonTerminals) {
+        auto &nt = nonTerminal.second;
+        for (int i = 0; i < nt.getProductions().size(); i++) {
+            auto &production = nt.getProductions()[i];
+            auto &firstSet = nt.getFirst()[i];
+            for (auto &terminal: firstSet) {
+                if (terminal->getIsEpsilon()) {
+                    for (auto &terminal: nt.getFollow()) {
+                        if(nt.getTransitions().find(terminal) == nt.getTransitions().end()) {
+                            nt.addTransition(terminal, production);
+                        }
+                        else {
+                            throw std::runtime_error("Conflict in parsing table");
+                        }
+                    }
+                } else {
+                    if(nt.getTransitions().find(terminal) == nt.getTransitions().end()) {
+                        nt.addTransition(terminal, production);
+                    }
+                    else {
+                        throw std::runtime_error("Conflict in parsing table");
+                    }
+                }
+            }
+        }
+    }
+}
--- ParserGenerator/ParserTable/ParserTable.h
+++ ParserGenerator/ParserTable/ParserTable.h
@@ -2,7 +2,19 @@
 #define PROJECT_PARSERTABLE_H
 
 
+#include <string>
+#include "../Utility/NonTerminal.h"
+
 class ParserTable {
+public:
+
+    std::map<std::string,NonTerminal> nonTerminals;
+
+    // Constructor
+    ParserTable(std::map<std::string,NonTerminal>& nonTerminals);
+
+    void parserTableCreator();
+
 
 };
 
